22522002adb8fb45e20de9f011a1b96fdd042a73
hacks.guide <128547338+Nomaakip@users.noreply.github.com>
2024-11-09 20:38:13 +0100

    Create km.cpp

---
+++ km.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+int yes;
+double miles;
+double km = 1.609;
+std::cout << "Put a number of miles plz greg\n";
+std::cin >> miles;
+std::cout << "The KM is " << miles * km << " So uh enjoy ig\n"; 
+std::cout << "now ur app is an fire exe\n";
+std::cin >> yes;
+
+}
